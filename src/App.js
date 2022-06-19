70c1e3a6176e2d494248a8089e2d3679bd616087
Darab <darabcode@gmail.com>
2022-06-19 17:14:21 +0100

    *Added new arts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BlogDisplay from "./Components/Blog";
 import Contact from "./Components/Contacts";
 import Footer from "./Components/Footer";
@@ -6,8 +6,13 @@ import Header from "./Components/Header";
 import Modal from "./Components/Modal";
 import Brand from "./Components/Testimonial";
 import logo from "./util";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
   return (
     <div>
       <Header logo={logo.second} />
--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -4,51 +4,69 @@ const blog = [
   {
     id: 1,
     images: "./images/beauty.jfif",
+    dataaos1: "flip-right",
+    datadelay: "400",
     name: "Art 1",
     detail:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod laborum voluptas placeat doloribus illo sint temporibus maxime modi unde velitLorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum animi, tempore rem deserunt alias maxime odio cupiditate non voluptate eum facilis distinctio ipsum assumenda omnis, qui aliquam eos repellat molestias impedit! Libero obcaecati consequatur, est adipisci architecto voluptatum molestiae id, perspiciatis eveniet enim vero? Aliquid ullam aliquam itaque fugiat facilis?",
   },
   {
     id: 2,
-    images: "./images/eyeball.jfif",
+    images: "./images/art2.jpg",
     name: "Art 2",
+    dataaos1: "flip-left",
+    datadelay: "400",
     detail:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod laborum voluptas placeat doloribus illo sint temporibus maxime modi unde velitLorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum animi, tempore rem deserunt alias maxime odio cupiditate non voluptate eum facilis distinctio ipsum assumenda omnis, qui aliquam eos repellat molestias impedit! Libero obcaecati consequatur, est adipisci architecto voluptatum molestiae id, perspiciatis eveniet enim vero? Aliquid ullam aliquam itaque fugiat facilis?",
   },
   {
     id: 3,
-    images: "./images/feather.jfif",
+    images: "./images/art3.jpg",
     name: "Art 3",
+    dataaos1: "fade-up",
+    datadelay: "400",
     detail:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod laborum voluptas placeat doloribus illo sint temporibus maxime modi unde velitLorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum animi, tempore rem deserunt alias maxime odio cupiditate non voluptate eum facilis distinctio ipsum assumenda omnis, qui aliquam eos repellat molestias impedit! Libero obcaecati consequatur, est adipisci architecto voluptatum molestiae id, perspiciatis eveniet enim vero? Aliquid ullam aliquam itaque fugiat facilis?",
   },
   {
     id: 4,
-    images: "./images/forest.jfif",
+    images: "./images/art4.jpg",
     name: "Art 4",
+    dataaos1: "flip-left",
+    datadelay: "400",
     detail:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod laborum voluptas placeat doloribus illo sint temporibus maxime modi unde velitLorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum animi, tempore rem deserunt alias maxime odio cupiditate non voluptate eum facilis distinctio ipsum assumenda omnis, qui aliquam eos repellat molestias impedit! Libero obcaecati consequatur, est adipisci architecto voluptatum molestiae id, perspiciatis eveniet enim vero? Aliquid ullam aliquam itaque fugiat facilis?",
   },
   {
     id: 5,
-    images: "./images/guy.jfif",
+    images: "./images/art5.jpg",
     name: "Art 5",
+    dataaos1: "flip-up",
+    datadelay: "400",
     detail:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod laborum voluptas placeat doloribus illo sint temporibus maxime modi unde velitLorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum animi, tempore rem deserunt alias maxime odio cupiditate non voluptate eum facilis distinctio ipsum assumenda omnis, qui aliquam eos repellat molestias impedit! Libero obcaecati consequatur, est adipisci architecto voluptatum molestiae id, perspiciatis eveniet enim vero? Aliquid ullam aliquam itaque fugiat facilis?",
   },
   {
     id: 6,
-    images: "./images/happy-girl.jfif",
+    images: "./images/art6.jpg",
     name: "Art 6",
+    dataaos1: "zoom-in",
+    datadelay: "400",
     detail:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod laborum voluptas placeat doloribus illo sint temporibus maxime modi unde velitLorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum animi, tempore rem deserunt alias maxime odio cupiditate non voluptate eum facilis distinctio ipsum assumenda omnis, qui aliquam eos repellat molestias impedit! Libero obcaecati consequatur, est adipisci architecto voluptatum molestiae id, perspiciatis eveniet enim vero? Aliquid ullam aliquam itaque fugiat facilis?",
   },
 ];
-const Blog = ({ images, name, detail }) => {
+const Blog = ({ images, name, detail, dataaos1, datadelay }) => {
   return (
     <div className="blogMedia">
-      <img src={images} alt="Art 1" />
-      <div>
+      <img
+        src={images}
+        alt="Art 1"
+        data-aos={dataaos1}
+        data-aos-delay={datadelay}
+      />
+
+      <div className="blogInfo" data-aos={dataaos1} data-aos-deley={datadelay}>
         <h1>{name}</h1>
         <p>{detail}</p>
         <button>EXPLORE</button>
@@ -71,6 +89,8 @@ const BlogDisplay = () => {
             images={index.images}
             name={index.name}
             detail={index.detail}
+            dataaos1={index.dataaos1}
+            datadelay={index.datadelay}
           />
         ))}
       </div>
